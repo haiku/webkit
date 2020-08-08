e9f948f834462ffbc5527e7b58d56cde872dc675
Adrien Destugues <pulkomandy@pulkomandy.tk>
2020-08-08 11:02:22 +0200

    Build fix.
    Hiaku now has an madvise implementation, so this results in conflicting
    defines, and can be removed.

--- Source/WTF/wtf/posix/OSAllocatorPOSIX.cpp
+++ Source/WTF/wtf/posix/OSAllocatorPOSIX.cpp
@@ -31,12 +31,6 @@
 #include <wtf/Assertions.h>
 #include <wtf/PageAllocation.h>
 
-#if OS(HAIKU)
-#define madvise posix_madvise
-#define MADV_DONTNEED POSIX_MADV_DONTNEED
-#define MADV_WILLNEED POSIX_MADV_WILLNEED
-#endif
-
 namespace WTF {
 
 void* OSAllocator::reserveUncommitted(size_t bytes, Usage usage, bool writable, bool executable, bool includesGuardPages)
